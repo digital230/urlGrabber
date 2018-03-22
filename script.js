348c754f61086e10ac23927d9057f50b41d47900
digital230 <digit230@gmail.com>
2018-03-23 02:38:22 +0500

    ability to grab srcset data

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function fetchLinks() {
-  var links = document.querySelectorAll('[src],[href]');
+  var links = document.querySelectorAll('img[src], img[srcset],a[href],[background-image]');
   if (links && links.length > 0) {
     createListing(links);
   }
@@ -8,13 +8,12 @@ function fetchLinks() {
 
 function createListing(links) {
   let arr = [];
+  let validItems = ['IMG', 'A', 'DIV'];
   for(var i = 0; i < links.length ; i++) {
     let item = links[i];
-    let src;
-    if (item.nodeName === 'IMG' || item.nodeName === 'A') {
-      src = item.getAttribute('src') || item.getAttribute('href');
-      let type = src.split('.').pop();
-      if (isValidUrl(src, type)) arr.push(src)
+    if (validItems.indexOf(item.nodeName) > -1 ) {
+      let {src, srcset} = extractUrlFromHtmlTags(item);
+      handleImagesUrlStacking(src, srcset, arr);
     }
   }
   if (arr.length > 0) {
@@ -41,17 +40,37 @@ function reload() {
 }
 
 function isValidUrl(url, type) {
-  let validExts = ['jpg', 'png', 'mp3', 'mp4', 'jpeg'];
-  if (validExts.indexOf(type) > -1) return true;
-  if (validExts.indexOf(type) === -1) {
+  let rgx = new RegExp("[$&+,:;=?@#|'<>.^*()%!-]");
+  let actualType = type.split(rgx)[0];
+  let validExts = ['jpg', 'png', 'mp3', 'mp4', 'jpeg', 'webp'];
+
+  if (validExts.indexOf(actualType) > -1) return true;
+  if (validExts.indexOf(actualType) === -1) {
     if (url.split(',')[0] === 'data:image/jpeg;base64') return true; // base64 img data
   } else {
     return false;
   }
-
 }
 
+function extractUrlFromHtmlTags(item) {
+  let srcset = [];
+  if (item.getAttribute('srcset')) {
+    srcset = item.getAttribute('srcset').split(',')
+  }
+  let src = item.getAttribute('src')
+  || item.getAttribute('href')
+  || item.getAttribute('background-image');
 
+  if (src) return {src, srcset};
+  return {src: 'no-src', srcset: []};
+}
+
+function handleImagesUrlStacking(src, srcset, arr) {
+  let type = src.split('.').pop();
+
+  if (isValidUrl(src, type)) arr.push(src);
+  if (!isValidUrl(src, type) && srcset.length > 0) Array.prototype.push.apply(arr, srcset);
+}
 
 
 
